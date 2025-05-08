4b0a8ff0180155b7b4970c2b5f81c71788b39025
LeonardoFcoBG <leonardofcobg@gmail.com>
2025-05-07 22:33:30 -0600

    Base proyect

---
+++ spec/language-gdscript-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageGdscriptView from '../lib/language-gdscript-view';
+
+describe('LanguageGdscriptView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
