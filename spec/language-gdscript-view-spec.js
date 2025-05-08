467e3922b0c7cfc8caf6ed2c04deb8d51c3de2e3
LeonardoFcoBG <leonardofcobg@gmail.com>
2025-05-08 07:43:11 -0600

    Eliminate unused elements

--- spec/language-gdscript-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageGdscriptView from '../lib/language-gdscript-view';
-
-describe('LanguageGdscriptView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
