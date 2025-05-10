9c8b4731e557d95dc6d72f32122c7ad063afba04
LeonardoFcoBG <leonardofcobg@gmail.com>
2025-05-09 22:49:33 -0600

    Cleaning unnecesary stuff

--- spec/language-gdscript-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import GDScriptView from '../lib/language-gdscript-view';
-
-describe('GDScriptView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
